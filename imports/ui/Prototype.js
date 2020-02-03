7a46f9edb49f973a64dc394ea36e9b536c9c9fbd
Andrew Liebchen <liebchen@fb.com>
2020-02-02 20:50:49 -0800

    Add aframe

--- imports/ui/Prototype.js
+++ imports/ui/Prototype.js
@@ -3,17 +3,21 @@ import { withTracker } from "meteor/react-meteor-data";
 import { Prototypes } from "../api/prototypes";
 import { Flex, Box, Text, Heading } from "rebass";
 import SceneList from "./SceneList";
+import Canvas from "./Canvas";
 
 const Prototype = props => (
   <Box>
     {props.prototype ? (
-      <Box>
+      <Flex>
         <Box>
-          <Heading>Prototype info</Heading>
-          <Text>ID: {props.prototype._id}</Text>
+          <Box>
+            <Heading>Prototype info</Heading>
+            <Text>ID: {props.prototype._id}</Text>
+          </Box>
+          <SceneList parentId={props.prototype._id} />
         </Box>
-        <SceneList parentId={props.prototype._id} />
-      </Box>
+        <Canvas />
+      </Flex>
     ) : (
       <Flex>Loading...</Flex>
     )}
